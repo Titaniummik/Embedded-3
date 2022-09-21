6429a7eb551e4a14d4321e11142a5b00e35778ec
Titaniummik <45991171+Titaniummik@users.noreply.github.com>
2022-09-21 09:45:00 +0200

    Updated requoirements and moved credentials to .env

--- api/settings.py
+++ api/settings.py
@@ -1,6 +1,9 @@
+from dotenv import dotenv_values
+Credentials = dotenv_values(".env")
+
 JWT_SECRET_KEY = 't1NP63m4wnBg6nyHYKfmc2TpCOGI4nss'
 CORS_HEADERS = 'Content-Type'
 MONGODB_SETTINGS = {
-    'host': 'mongodb+srv://API:SsbEioMHqfWHXStd@cluster0.nvhyrvk.mongodb.net/?retryWrites=true&w=majority'    
+    'host': 'mongodb+srv://{}:{}@cluster0.nvhyrvk.mongodb.net/?retryWrites=true&w=majority' .format(Credentials["user"], Credentials["password"])   
 }
 PROPAGATE_EXCEPTIONS = True
